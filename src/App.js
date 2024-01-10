ca3f9ba7d12323dd2745ce9f1d9cf3cae9bc30af
ndjerrou <n.djerrou@gmail.com>
2024-01-10 12:21:12 +0100

    example of state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [theme, setTheme] = useState('dark');
+
+  const handleClick = () => {
+    alert('tu viens de cliquer sur le bouton');
+  };
+
+  //   let theme = 'dark';
+  //   console.log('top level theme = ', theme);
+
+  const calculateBgColor = () => {
+    return theme === 'dark' ? 'green' : 'gold';
+  };
+
+  //   const handleTheme = () => {
+  //     if (theme === 'dark') {
+  //       theme = 'light';
+  //     } else {
+  //       theme = 'dark';
+  //     }
+
+  //     // console.log('handletheme fired : ', theme);
+  //   };
+  const handleTheme = () => {
+    setTheme(prevState => {
+      if (theme === 'dark') {
+        return 'light';
+      }
+      return 'dark';
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1
+        style={{
+          backgroundColor: calculateBgColor(),
+        }}
+      >
+        Un peu d'interaction !!
+      </h1>
+      <button onClick={handleClick}>click me</button>
+      <button onMouseOver={() => console.log('on mouse over fired')}>
+        come here
+      </button>
+      <button onClick={handleTheme}>Change bgColor du h1</button>
+    </>
   );
-}
+};
 
 export default App;
+
+// document.querySelector('unelement').addEventListener('click', () => {
+
+// })
